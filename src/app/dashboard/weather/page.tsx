f641c8f15f78dd5bfd0b88a1539aa99bbc156db2
chantalchung <chantaalchung@gmail.com>
2024-06-11 18:00:34 -0700

    add placeholder search and location button components

--- src/app/dashboard/weather/page.tsx
+++ src/app/dashboard/weather/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { WEATHER_API_URL } from '@/src/app/lib/api';
-import { Card } from '@/src/app/ui/dashboard/cards';
 import { CardSkeleton } from '@/src/app/ui/skeletons';
 // import { Metadata } from 'next';
 import { Suspense, useEffect, useState } from 'react';
+import Card from '@/src/app/ui/components/Card';
+import { lusitana } from '@/src/app/ui/fonts';
+import Button from '@/src/app/ui/components/Button';
+import { MdMyLocation, MdOutlineLocationOn } from 'react-icons/md';
+import Search from '@/src/app/ui/components/Search';
 
 // export const metadata: Metadata = {
 //   title: 'Weather',
@@ -28,8 +32,23 @@ export default function Page() {
   }, []);
 
   return (
-    <Suspense fallback={<CardSkeleton />}>
-      <Card title="Weather" value={JSON.stringify(data)} type="invoices" />
-    </Suspense>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Weather</h1>
+        <h2 className="text-l flex items-center gap-2">
+          Current Location: Vancouver
+          <MdOutlineLocationOn />
+        </h2>
+      </div>
+      <div className="mb-4 mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search location..." />
+        <Button>
+          <MdMyLocation />
+        </Button>
+      </div>
+      <Suspense fallback={<CardSkeleton />}>
+        <Card title="Weather" value={JSON.stringify(data)} type="weather" />
+      </Suspense>
+    </div>
   );
 }
