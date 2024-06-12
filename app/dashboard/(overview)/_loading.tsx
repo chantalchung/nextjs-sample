d5276677616a254330160dd6a78bc3409a4172c2
chantalchung <chantaalchung@gmail.com>
2024-06-11 17:22:03 -0700

    refactor: move to src folder

--- app/dashboard/(overview)/_loading.tsx
+++
@@ -1,6 +0,0 @@
-import DashboardSkeleton from '@/app/ui/skeletons';
-
-// not in use - should be just named loading.tsx to stream the whole page
-export default function Loading() {
-  return <DashboardSkeleton />;
-}
---
+++ src/app/dashboard/(overview)/_loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from '@/src/app/ui/skeletons';
+
+// not in use - should be just named loading.tsx to stream the whole page
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
